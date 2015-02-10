d91561e105058e15c302e4fe08c94603c7be1168
Josh Malkinson <josh.malkinson@codethink.co.uk>
2015-02-10 19:06:15 +0000

    test_roman_numeral.py: add test_five through _ten

--- test_roman_numeral.py
+++ test_roman_numeral.py
@@ -36,6 +36,32 @@ class RomanNumeralTestCase(unittest.TestCase):
         result = self.testclass.generator(5)
         self.assertEqual(result, 'V')
 
+    def test_six(self):
+        """ Does 6 return VI? """
+        result = self.testclass.generator(6)
+        self.assertEqual(result, 'VI')
+        
+    def test_seven(self):
+        """ Does 7 return VII? """
+        result = self.testclass.generator(7)
+        self.assertEqual(result, 'VII')
+
+    def test_eight(self):
+        """ Does 8 return VIII? """
+        result = self.testclass.generator(8)
+        self.assertEqual(result, 'VIII')
+
+    def test_nine(self):
+        """ Does 9 return IX? """
+        result = self.testclass.generator(9)
+        self.assertEqual(result, 'IX')
+
+    def test_ten(self):
+        """ Does 10 return X? """
+        result = self.testclass.generator(10)
+        self.assertEqual(result, 'X')
+
+
 if __name__ == '__main__':
     unittest.main()
 
